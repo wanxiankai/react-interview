b8cb55cad8e538977240cb4670405d8381abe334
wanxiankai <wanxiankai@126.com>
2024-01-12 13:36:51 +0800

    feat: format num

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,12 @@
 export function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
 }
+
+
+export function formatCount(count:number){
+  if(count < 1000){
+    return count.toString();
+  }else {
+    return (count/1000).toFixed(1) + 'k'
+  }
+}
